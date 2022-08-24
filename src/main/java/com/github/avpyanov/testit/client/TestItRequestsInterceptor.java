69a6f68958da24d9cf1424538fbd6759751db06d
apyanov <>
2022-08-24 12:16:14 +0300

    request interceptor

--- src/main/java/com/github/avpyanov/testit/client/TestItRequestsInterceptor.java
+++ src/main/java/com/github/avpyanov/testit/client/TestItRequestsInterceptor.java
@@ -3,7 +3,6 @@ package com.github.avpyanov.testit.client;
 import okhttp3.Interceptor;
 import okhttp3.Request;
 import okhttp3.Response;
-import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 
@@ -15,9 +14,8 @@ public class TestItRequestsInterceptor implements Interceptor {
         this.privateToken = privateToken;
     }
 
-    @NotNull
     @Override
-    public Response intercept(@NotNull Chain chain) throws IOException {
+    public Response intercept(Chain chain) throws IOException {
         Request request = chain.request();
         Request requestWithAuthorization = request.newBuilder()
                 .addHeader("Authorization", "PrivateToken " + privateToken)
