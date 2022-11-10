5b1aaa588ccd194aab3d384406e737f20d361208
apyanov <>
2022-11-10 15:26:31 +0300

    fix id type to UUID

--- src/main/java/com/github/avpyanov/testit/client/dto/Attachment.java
+++ src/main/java/com/github/avpyanov/testit/client/dto/Attachment.java
@@ -2,8 +2,10 @@ package com.github.avpyanov.testit.client.dto;
 
 import lombok.Data;
 
+import java.util.UUID;
+
 @Data
 public class Attachment {
 
-    private String id;
+    private UUID id;
 }
